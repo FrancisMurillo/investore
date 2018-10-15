bafeee1599c34fbc8b55a14eb622571e1740a0cd
Francis Murillo <francis@invento.io>
2018-10-15 10:56:51 +0800

    Rename investore

--- apps/investore_products/test/invenstore_products_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule InvenstoreProductsTest do
-  use ExUnit.Case
-  doctest InvenstoreProducts
-
-  test "greets the world" do
-    assert InvenstoreProducts.hello() == :world
-  end
-end
---
+++ apps/investore_products/test/investore_products_test.exs
@@ -0,0 +1,7 @@
+defmodule InvestoreProductsTest do
+  use ExUnit.Case
+
+  test "greets the world" do
+    assert InvestoreProducts.hello() == :world
+  end
+end
